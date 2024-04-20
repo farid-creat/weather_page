ae616e1c7b0c7a47222864b33d2884bf79643cd2
farid <faridnemati48@gmail.com>
2024-04-20 06:02:27 -0700

    add picture to the background and set the positions and make box for 3 days forecast

--- src/App.js
+++ src/App.js
@@ -3,40 +3,41 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
 
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-4">
-            <div class="search-bar">
-              <input type="text" class="form-control form-control-lg mt-4" placeholder="Enter Location..."></input>
-              <button class="btn btn-primary btn-lg mt-3">Search</button>
-            </div>
-          </div>
+      <div class="mycontainer">
+        <div class="row weather-info">
+
           <div class="col-lg-8">
-            <div class="weather-info text-center p-4">
-              <h1>Weather Prediction 🌦️</h1>
+            <div class="text-center p-4">
+              <h1>Weather Prediction</h1>
 
               <div class="current-weather mt-4">
-                <h2 class="location">New York, NY</h2>
-                <p class="temperature">25°C</p>
+                <h2 class="location pt-5">New York, NY</h2>
+                <p class="temperature pt-2">25°C</p>
                 <p class="description">Partly Cloudy</p>
               </div>
 
               <div class="future-weather mt-4">
                 <h3>Next 3 Days Forecast:</h3>
-                <div class="row mt-3">
-                  <div class="col">
+                <div class="row mt-5">
+                  <div class="col future-info">
                     <p><strong>Monday:</strong> 23°C, Sunny</p>
                   </div>
-                  <div class="col">
+                  <div class="col future-info">
                     <p><strong>Tuesday:</strong> 22°C, Rainy</p>
                   </div>
-                  <div class="col">
+                  <div class="col future-info">
                     <p><strong>Wednesday:</strong> 21°C, Cloudy</p>
                   </div>
                 </div>
               </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="search-bar text-center">
+              <input type="text" class="form-control form-control-lg mt-4" placeholder="Enter Location..."></input>
+              <button class="btn btn-primary btn-lg mt-3">Search</button>
             </div>
           </div>
         </div>
