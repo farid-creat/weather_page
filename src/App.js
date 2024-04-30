16d109a4c2635c46c1630b508bb291ac7c9ff198
farid <faridnemati48@gmail.com>
2024-04-30 06:03:09 -0700

    add forecast url and add error handler

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState ,useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 function App() {
-  const [location , setlocation] = useState("london")
-  const [temperature,settemperature] = useState()
-  const [description ,setvdescription] = useState("")
-  const [data ,setData] = useState()
+  const [location, setlocation] = useState("london")
+  const [temperature, settemperature] = useState()
+  const [error, setError] = useState(0)
+  const [data, setData] = useState()
+  const [forecast , setForecast] = useState()
   const url = `http://api.weatherapi.com/v1/current.json?key=7d9efb1a9b4d4a7cb51103832242404&q=${location}&aqi=no`
+  const forecasr_url = `http://api.weatherapi.com/v1/forecast.json?key=7d9efb1a9b4d4a7cb51103832242404&q=${location}&days=3&aqi=no&alerts=no`
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,9 +20,20 @@ function App() {
         console.error('Error fetching data:', error);
       }
     };
+
+    const fetchForecast = async () => {
+      try {
+        const response = await axios.get(forecasr_url);
+        setForecast(response.data); // Update the state with fetched data
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchForecast();
     fetchData();
   }, []);
-  if (!data) {
+  if (!data || !forecast) {
     return <div>Loading...</div>;
   }
 
@@ -30,14 +43,21 @@ function App() {
 
 
 
-  const handleSearch = () =>{
-    console.log("tessssssssssssssssssssssssss")
-    axios.get(url).then( res =>{
-      setData(res.data)
-      console.log(data)
-    })
-  }
-
+  const handleSearch = () => {
+    axios.get(url)
+      .then(res => {
+        setData(res.data);
+        setError(0); // Reset error state
+        return axios.get(forecasr_url); // Return the forecast request promise
+      })
+      .then(res1 => {
+        setForecast(res1.data);
+      })
+      .catch(error => {
+        console.error("Error:", error);
+        setError(1); // Set error status if available
+      });
+  };
 
 
 
@@ -50,7 +70,9 @@ function App() {
         <div className="row weather-info">
 
           <div className="col-lg-8">
-            <div className="text-center p-4">
+            {error == 1 && (<div className="text-center p-4"> <h1>Unknown place</h1> </div>)}
+
+            {error == 0 && (<div className="text-center p-4">
               <h1>Weather Prediction</h1>
 
               <div className="current-weather mt-5">
@@ -63,27 +85,28 @@ function App() {
                 <h3>Next 3 Days Forecast:</h3>
                 <div className="row mt-5">
                   <div className="col future-info">
-                    <p><strong>Monday</strong></p>
-                    <p>23°C</p>
-                    <p>Sunny</p>
+                    <p><strong>{forecast.forecast.forecastday[0].date}</strong></p>
+                    <p>{forecast.forecast.forecastday[0].day.avgtemp_c}°C</p>
+                    <p>{forecast.forecast.forecastday[0].day.condition.text}</p>
                   </div>
                   <div className="col future-info">
-                    <p><strong>Monday</strong></p>
-                    <p>23°C</p>
-                    <p>Sunny</p>
+                  <p><strong>{forecast.forecast.forecastday[1].date}</strong></p>
+                    <p>{forecast.forecast.forecastday[1].day.avgtemp_c}°C</p>
+                    <p>{forecast.forecast.forecastday[1].day.condition.text}</p>
                   </div>
                   <div className="col future-info">
-                    <p><strong>Monday</strong></p>
-                    <p>23°C</p>
-                    <p>Sunny</p>
+                  <p><strong>{forecast.forecast.forecastday[2].date}</strong></p>
+                    <p>{forecast.forecast.forecastday[2].day.avgtemp_c}°C</p>
+                    <p>{forecast.forecast.forecastday[2].day.condition.text}</p>
                   </div>
                 </div>
               </div>
-            </div>
+            </div>)}
+
           </div>
           <div className="col-lg-4">
             <div className="search-bar text-center">
-              <input type="text" class="form-control form-control-lg mt-4" placeholder="Enter Location..."value={location} onChange={handleInputChange}></input>
+              <input type="text" class="form-control form-control-lg mt-4" placeholder="Enter Location..." value={location} onChange={handleInputChange}></input>
               <button className="btn btn-primary btn-lg mt-3" onClick={handleSearch}>Search</button>
             </div>
           </div>
